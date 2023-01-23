77e29e027d399087f5f56b4f25c4ce77bfa5bffb
hk_117 <zubayer11704@gmail.com>
2023-01-23 19:44:47 +0600

    Static folder setup done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 5000;
 
 app.get('/',(req,res)=>{
