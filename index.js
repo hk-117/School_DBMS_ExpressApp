363e48791d2e7cd9d552960bcb18e38674fa6a8d
hk_117 <zubayer11704@gmail.com>
2023-01-23 19:54:39 +0600

    Basic routing added

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 const port = 5000;
 
+app.get('/teachers/:id',(req,res)=>{
+    res.send("<h1> Teacher Information </h1>");
+});
+
+app.get('/teachers',(req,res)=>{
+    res.send("<h1> Teachers </h1>");
+});
+
+app.get('/students/:id',(req,res)=>{
+    res.send("<h1> Student Information </h1>");
+});
+
+app.get('/students',(req,res)=>{
+    res.send("<h1> Students </h1>");
+});
+
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.send("<h1> Welcome. You are in Homepage. </h1>");
 });
 
 app.listen(port,()=>{
